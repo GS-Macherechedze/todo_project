c08b0dc988898b459bc422746b657b4423198bca
mnzyokaeric@gmail.com <mnzyokaeric@gmail.com>
2024-04-19 17:40:03 +0300

    python django and SQL intergrate project

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect
 from .models import Task
 
+# def task_home(request):
+#     tasks = Task.objects.all()
+#     return render(request, 'todo_list/index.html', {'tasks': tasks})
+
 def task_list(request):
     tasks = Task.objects.all()
     return render(request, 'todo_list/task_list.html', {'tasks': tasks})
